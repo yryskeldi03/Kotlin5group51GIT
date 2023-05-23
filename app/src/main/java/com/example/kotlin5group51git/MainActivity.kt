46d97eefc29b4494b8b8edd186eedb8f91901816
ymajrambekov@gmail.com <ymajrambekov@gmail.com>
2023-05-23 19:48:32 +0600

    added comments

--- app/src/main/java/com/example/kotlin5group51git/MainActivity.kt
+++ app/src/main/java/com/example/kotlin5group51git/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    // task1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    // task1
 }
