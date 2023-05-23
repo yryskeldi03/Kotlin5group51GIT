d97f62bc9de8c585cb7c2b7b3c07646063c44edd
ymajrambekov@gmail.com <ymajrambekov@gmail.com>
2023-05-23 20:22:34 +0600

    lalddjffdjkfld

--- app/src/main/java/com/example/kotlin5group51git/MainActivity.kt
+++ app/src/main/java/com/example/kotlin5group51git/MainActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     // task1
+    // dfjkldfjld
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
     // task1
+    // djfld
 }
