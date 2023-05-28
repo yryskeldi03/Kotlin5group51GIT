080b262c4db8eef6fd6ac110ea50f8f747618fa6
ymajrambekov@gmail.com <ymajrambekov@gmail.com>
2023-05-28 17:33:13 +0600

    clean project

--- app/src/main/java/com/example/kotlin5group51git/MainActivity.kt
+++ app/src/main/java/com/example/kotlin5group51git/MainActivity.kt
@@ -4,16 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    // task2
-    // task1
-    // jdlkfdjlkf
-    // dfjkldfjld
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    // hkdfkjhdfj
-    // task1
-    // djfkldjf
-    // djfld
+
 }
